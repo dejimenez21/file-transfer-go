e60cfddee69e86de2ed16edab99591f03a984583
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2022-08-24 22:26:15 -0400

    Update demo setup script

--- setupDemo.bat
+++ setupDemo.bat
@@ -1,6 +1,16 @@
 @echo off
 cd src\client\
 go build
+rmdir /s /q "C:\CFTP-DEMO\1\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\2\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\3\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\4\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\5\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\6\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\7\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\8\ReceivedFiles\"
+rmdir /s /q "C:\CFTP-DEMO\9\ReceivedFiles\"
+
 xcopy client.exe "C:\CFTP-DEMO\1\" /y  
 xcopy client.exe "C:\CFTP-DEMO\2\" /y
 xcopy client.exe "C:\CFTP-DEMO\3\" /y
@@ -10,3 +20,5 @@ xcopy client.exe "C:\CFTP-DEMO\6\" /y
 xcopy client.exe "C:\CFTP-DEMO\7\" /y
 xcopy client.exe "C:\CFTP-DEMO\8\" /y
 xcopy client.exe "C:\CFTP-DEMO\9\" /y
+
+
